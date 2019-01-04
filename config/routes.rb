ae974b47d32aafa8731313015343b6661f7616c5
Tammy Metz <tammy@tammymetz.com>
2019-01-04 15:53:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  #resources :posts, only: [:index, :show, :new, :create, :edit]
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :edit]
   patch 'posts/:id', to: 'posts#update'
 end
